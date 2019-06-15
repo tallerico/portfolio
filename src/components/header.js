915803e59717475559ad4f4b0e3598a3b8ea63c1
Mike Tallerico <mtallerico1@gmail.com>
2019-06-15 15:57:09 -0400

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Particles from './particles';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	position: relative;
+	width: 100vw;
+	height: 100vh;
+	background: #343434;
+`;
+
+const TextContainer = styled.div`
+	position: absolute;
+	display: flex;
+	flex-direction: column;
+	width: 300px;
+	left: 50%;
+	margin-left: -150px;
+	top: 50%;
+	margin-top: -150px;
+`;
+
+const HeaderOne = styled.h1`
+	color: white;
+`;
+
+const Paragraph = styled.p`
+	color: white;
+`;
+
+function header() {
+	return (
+		<Container>
+			<TextContainer>
+				<HeaderOne>Hi! I am Mike.</HeaderOne>
+				<Paragraph>Thank you so much for checking out my portfolio.</Paragraph>
+			</TextContainer>
+			<Particles />
+		</Container>
+	);
+}
+
+export default header;
