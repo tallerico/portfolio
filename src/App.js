1e8ea527762b62a08de18a836ee67bafc4c18cfa
Mike Tallerico <mtallerico1@gmail.com>
2019-06-15 21:01:55 -0400

    imports navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from './components/header';
+import Navigation from './components/navigation/navigation';
+import { createGlobalStyle } from 'styled-components';
 import './App.css';
 
+const GlobalStyles = createGlobalStyle`
+  body {
+    overflow: hidden;
+	}
+`;
+
 function App() {
-	return <Header />;
+	return (
+		<Fragment>
+			<GlobalStyles />
+			<Navigation />
+			<Header />
+		</Fragment>
+	);
 }
 
 export default App;
