7c591af8fc66d223f5f8006346ca3e2f2689b08c
Mike Tallerico <mtallerico1@gmail.com>
2019-06-16 22:00:25 -0400

    removed useCallback from handleClick function

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import { Transition } from 'react-transition-group';
+import Menu from './Menu';
 import Circle from './Circle';
-import styled from 'styled-components';
 
 function Navigation() {
 	const [clicked, setClicked] = useState(false);
 
-	const handleClick = useCallback(() => {
+	function handleClick() {
 		if (clicked) {
 			setClicked(false);
 		} else {
 			setClicked(true);
 		}
-	});
+	}
 
 	return (
 		<div>
+			<Menu clicked={clicked} />
 			<Transition in={clicked} timeout={500}>
 				{state => (
 					// state change: exited -> entering -> entered -> exiting -> exited
