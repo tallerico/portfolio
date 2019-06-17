cad8086a305cb9ddbaa298d87f6bfddcade84da4
Mike Tallerico <mtallerico1@gmail.com>
2019-06-16 22:04:44 -0400

    initial commit for About section

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from './Container';
+import InfoContainer from './InfoContainer';
+
+function About() {
+	return (
+		<Container>
+			<InfoContainer />
+		</Container>
+	);
+}
+
+export default About;
