7ae61e95aae13a5797f596f68c6dd263082b9687
Mike Tallerico <mtallerico1@gmail.com>
2019-06-17 20:33:09 -0400

    adjusted max width

--- src/components/about/InfoContainer.js
+++ src/components/about/InfoContainer.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
 const InfoContainer = styled.div`
-	padding: 2em;
+	padding: 1em;
 	background-color: #080ae3;
+	border-radius: 10px;
+	max-width: 600px;
+	margin-left: auto;
+	margin-right: auto;
 `;
 
 export default InfoContainer;
