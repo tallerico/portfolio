2119e9c201836c60c6bca6b224c902ba58bab4a7
Mike Tallerico <mtallerico1@gmail.com>
2019-06-17 20:32:50 -0400

    link block for about component

---
+++ src/components/about/LinkBock.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import github from './001-github-logo.svg';
+import linkedin from './002-linkedin.svg';
+
+const Container = styled.div`
+	display: flex;
+`;
+
+const Icon = styled.img`
+	height: 40px;
+	margin-right: 10px;
+`;
+
+function LinkBlock() {
+	return (
+		<Container>
+			<a href="https://www.github.com/tallerico">
+				<Icon src={github} alt="github icon" />
+			</a>
+			<a href="https://www.linkedin.com/in/mike-tallerico-03580339/">
+				<Icon src={linkedin} alt="linked icon" />
+			</a>
+		</Container>
+	);
+}
+
+export default LinkBlock;
