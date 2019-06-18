1748104c1e010de8b54d7336528ba0b1854e9e59
Mike Tallerico <mtallerico1@gmail.com>
2019-06-17 20:34:11 -0400

    removed unused styled components

--- src/components/header.js
+++ src/components/header.js
@@ -7,30 +7,6 @@ const Container = styled.div`
 	overflow: hidden;
 `;
 
-const TextContainer = styled.div`
-	position: absolute;
-	display: flex;
-	flex-direction: column;
-	width: 300px;
-	left: 50%;
-	margin-left: -150px;
-	top: 50%;
-	margin-top: -150px;
-`;
-
-const HeaderOne = styled.h1`
-	color: white;
-`;
-
-const Paragraph = styled.p`
-	color: white;
-`;
-
-const BlueDiv = styled.div`
-	height: 1px;
-	background-color: #080ae3;
-`;
-
 function header() {
 	return (
 		<Container>
