2ae309e97612b00ecacc87097bca1752ba55fa5a
Mike Tallerico <mtallerico1@gmail.com>
2019-06-21 00:03:17 -0400

    changed font size

--- src/components/base/Paragraph.js
+++ src/components/base/Paragraph.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Paragraph = styled.p.attrs(props => {})`
 	color: ${props => props.color};
-	font-size: 0.8em;
+	font-size: 1em;
 	font-weight: 400;
 `;
 
