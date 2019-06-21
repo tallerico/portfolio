b587b35b1e184545fb272f3e385a9225eb7b9782
Mike Tallerico <mtallerico1@gmail.com>
2019-06-21 00:02:05 -0400

    adjusted background color

--- src/components/about/Container.js
+++ src/components/about/Container.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-	padding: 3em 1em 1em 1em;
+	padding: 3em 1em 3em 1em;
+	background-color: #f5f2f2;
 `;
 
 export default Container;
