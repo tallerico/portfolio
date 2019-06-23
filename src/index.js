57f5c062654ecfb5da3f061215078ee320a6e582
Mike Tallerico <mtallerico1@gmail.com>
2019-06-23 11:16:31 -0400

    removed unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as V from 'victory';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
